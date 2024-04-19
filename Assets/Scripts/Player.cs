6317af509791e64969643ee36ec68eb6a7894ca6
Chloeliyi <s10247578@connect.np.edu.sg>
2024-04-19 23:11:22 +0800

    tmp pro

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,8 +25,8 @@ public class FPSController : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
     }
 
     // Update is called once per frame
