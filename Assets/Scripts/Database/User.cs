3d7c2c4509133769d4fc11d55c4d00f4de650bfb
JunYangZhu <116061020+JunYangZhu@users.noreply.github.com>
2024-08-04 14:11:16 +0800

    database script

--- Assets/Scripts/Database/User.cs
+++ Assets/Scripts/Database/User.cs
@@ -3,15 +3,13 @@ public class User {
     public string email;
     public string password;
     public bool userStatus;
-    public string pfp;
     public User() {
     }
 
-    public User(string userName, string email, string password, bool userStatus, string pfp = "default") {
+    public User(string userName, string email, string password, bool userStatus) {
         this.userName = userName;
         this.email = email;
         this.password = password;
         this.userStatus = userStatus;
-        this.pfp = pfp;
     }
 }
